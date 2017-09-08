80900bef3d386c9269c83c3fdbc50f2c0df2ab8d
John Doe <cspeer@usc.edu>
2017-09-07 21:57:44 -0700

    set up basic components structure

---
+++ docs/scripts/components.js
@@ -0,0 +1,4 @@
+Vue.component( "tally-list" , {
+	template: `
+	<div class='equation-list'>1st component</div>`
+})
---
+++ docs/scripts/runner.js
@@ -0,0 +1,5 @@
+function load (){
+	return [{description: "test", count: 0}];
+};
+
+new Vue({el: "#tally-list"})
