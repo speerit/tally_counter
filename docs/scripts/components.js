09307d558cd54750f5a330c69c68cdd7e97ec343
Colette Speer <cspeer@usc.edu>
2017-09-21 19:52:52 -0700

    mvp on tally block component

--- docs/scripts/components.js
+++ docs/scripts/components.js
@@ -1,4 +1,32 @@
 Vue.component( "tally-list" , {
 	template: `
-	<div class='equation-list'>1st component</div>`
+	<div class='tally-list'>
+    <h2>First Component</h2>
+    <tally-block v-for='(tallyData, index) in tallies'
+      v-bind:key=index
+      v-bind:tallyData=tallyData>
+    </tally-block>
+  </div>
+  `,
+  data: function (){return {tallies: load()}},
+})
+
+Vue.component( "tally-block" , {
+  template: `
+  <div class='tally-block'>
+    <h3>{{tallyData.description}}</h3>
+    <div class='count-block'>
+      <h3>Done {{tallyData.count}} times.</h3>
+      <button v-on:click='countPlusPlus'>
+        +1
+      </button>
+    </div>
+  </div>
+  `,
+  props:["tallyData"],
+  methods:{
+    countPlusPlus: function(){
+      this.tallyData.count += 1
+    }
+  }
 })
--- docs/scripts/runner.js
+++ docs/scripts/runner.js
@@ -1,5 +1,6 @@
+
 function load (){
-	return [{description: "test", count: 0}];
+	return [{description: "test", count: 0}, {description: "test2", count: 0}, {description: "test3", count: 3}];
 };
 
 new Vue({el: "#tally-list"})
