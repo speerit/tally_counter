f30aa45c78e742416e392c25a18b5e4520aba500
wolfram74 <phaugen@gmail.com>
2017-09-21 21:17:19 -0600

    renamed state variable, provided reasonable default value for new user

--- docs/scripts/runner.js
+++ docs/scripts/runner.js
@@ -2,12 +2,12 @@
 function load (){
 	// return [{description: "test", count: 0}, {description: "test2", count: 0}, {description: "test3", count: 3}];
   return JSON.parse(
-    localStorage.getItem('state')
-  )
+    localStorage.getItem('tallyState')
+  )|| [{description:"make new tally counter", count: 0}]
 };
 
 function save(array){
-  localStorage.setItem('state',
+  localStorage.setItem('tallyState',
     JSON.stringify(array)
   )
 }
